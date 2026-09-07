ce33deaabdaf4d77ee60564ed60be16782ad04e2
MMehediMasum <iammehedimasum@gmail.com>
2026-09-07 19:09:58 +0600

    Initial site MMehediMasum

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  // Set this to your production URL (no trailing slash)
+  site: 'https://mmehedimasum.com',
+  // Set this to your site's subpath if it is NOT hosted at the domain root
+  // (e.g. GitHub Pages project sites). Delete this line if your site lives at "/".
+  integrations: [sitemap()],
+});
